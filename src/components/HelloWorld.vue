615fba20b15aa14e81e7fc408b481aa334d0f54d
Evgeny <kholoshax@mail.ru>
2024-01-08 21:20:17 +0300

    Базовые настройки EsLint и Prettier

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,33 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref } from 'vue';
 
-defineProps<{ msg: string }>()
+interface IProps {
+  msg?: string;
+}
 
-const count = ref(0)
+withDefaults(defineProps<IProps>(), {
+  msg: 'qwe',
+});
+
+const count = ref(0);
+const tmp = 'qwe';
+console.log('tmp ', tmp);
+const check = (qwe: string) => qwe;
+check('qwe');
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <button
+      type="button"
+      class="button-classssssssssssssssssssssssssssssssssssssssssss"
+      id="button-id"
+      @click="count++"
+    >
+      count is {{ count }}
+    </button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
@@ -19,9 +36,8 @@ const count = ref(0)
 
   <p>
     Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
+    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank">create-vue</a>, the official Vue + Vite
+    starter
   </p>
   <p>
     Install
